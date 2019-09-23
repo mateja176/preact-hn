22b18518de82c2a0d760430538af6f07fb50ba30
Mateja Petrovic <ma.petrovic@levi9.com>
2019-09-23 21:43:00 +0200

    feat(component/Comments): adds ability to collapse comments

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -2,20 +2,45 @@ import React from 'react';
 import { IComment } from '../models';
 import Comments from './Comments';
 
-const Comment: React.FC<Omit<IComment, 'id'>> = ({ by, text, kids = [] }) => (
-  <>
-    <i>{by}</i>
-    <p dangerouslySetInnerHTML={{ __html: text }} />
-    <div
-      style={{
-        borderLeft: '1px solid lightgrey',
-        marginLeft: 10,
-        paddingLeft: 10,
-      }}
-    >
-      <Comments commentsIds={kids} />
-    </div>
-  </>
-);
+const Comment: React.FC<Omit<IComment, 'id'>> = ({ by, text, kids = [] }) => {
+  const [collapsed, setCollapsed] = React.useState(false);
+
+  const toggleCollapsed = () => {
+    setCollapsed(!collapsed);
+  };
+
+  return (
+    <>
+      <div>
+        <i
+          onClick={toggleCollapsed}
+          onKeyPress={toggleCollapsed}
+          tabIndex={0}
+          role="button"
+          style={{ cursor: 'pointer' }}
+        >
+          {collapsed ? '▼' : '▲'}
+        </i>
+        <i>{by}</i>
+      </div>
+      <div
+        style={{
+          display: collapsed ? 'none' : 'initial',
+        }}
+      >
+        <p dangerouslySetInnerHTML={{ __html: text }} />
+        <div
+          style={{
+            borderLeft: '1px solid lightgrey',
+            marginLeft: 10,
+            paddingLeft: 10,
+          }}
+        >
+          <Comments commentsIds={kids} />
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Comment;
