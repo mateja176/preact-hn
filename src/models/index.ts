1b02200889ded7bde939185a66d167ca000bff1c
Mateja Petrovic <ma.petrovic@levi9.com>
2019-09-23 17:39:33 +0200

    feat: adds models

---
+++ src/models/index.ts
@@ -0,0 +1,22 @@
+export type Id = number;
+export type Ids = Id[];
+
+export interface IStory {
+  id: Id;
+  title: string;
+  kids?: Ids;
+  score: number;
+  url: string;
+  by: string;
+}
+
+export type IStories = IStory[];
+
+export interface IComment {
+  id: Id;
+  text: string;
+  by: string;
+  kids: Ids;
+}
+
+export type IComments = IComment[];
