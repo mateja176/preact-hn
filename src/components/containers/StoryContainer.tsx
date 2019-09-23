ce4573292497012f37c3cbfcddda8ae063062d28
Mateja Petrovic <ma.petrovic@levi9.com>
2019-09-23 20:34:31 +0200

    feat(components): adds StoryContainer

--- src/components/containers/StoryContainer.tsx
+++ src/components/containers/StoryContainer.tsx
@@ -8,7 +8,10 @@ import {
 } from 'react-async';
 import { IStory, WithId } from '../../models';
 import { fetchJSON } from '../../utils';
+import MessageContainer from '../MessageContainer';
 import Story from '../Story';
+
+const minStoryHeight = 46;
 
 const fetchItem = ({ id }: AsyncProps<IStory>) => fetchJSON(`/item/${id}.json`);
 
@@ -17,9 +20,17 @@ const StoryContainer: React.FC<WithId> = ({ id }) => {
 
   return (
     <>
-      <IfPending state={state}>Loading story...</IfPending>
+      <IfPending state={state}>
+        <MessageContainer height={minStoryHeight}>
+          Loading story...
+        </MessageContainer>
+      </IfPending>
       <IfRejected state={state}>
-        {() => <>Error while loading story, please retry.</>}
+        {() => (
+          <MessageContainer height={minStoryHeight}>
+            Error while loading story, please retry.
+          </MessageContainer>
+        )}
       </IfRejected>
       <IfFulfilled state={state}>
         {story => <Story {...story}></Story>}
