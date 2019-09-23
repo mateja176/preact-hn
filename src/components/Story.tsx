e8d7740ee49f0f95db8241ed792d1170cab9d810
Mateja Petrovic <ma.petrovic@levi9.com>
2019-09-23 17:42:55 +0200

    feat(components): adds Story

---
+++ src/components/Story.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { IStory } from '../models';
+
+const Story: React.FC<IStory> = story => {
+  const { id, title, url, by, score, kids = [] } = story;
+
+  return (
+    <>
+      <a href={url} target="_blank" rel="noopener noreferrer">
+        <h2>
+          {title} <small>({new URL(url).hostname})</small>
+        </h2>
+      </a>
+      <div>
+        <strong>{score}</strong> points by <i>{by}</i>
+        {!!kids.length && (
+          <>
+            {' '}
+            |{' '}
+            <Link
+              to={{
+                pathname: `/${id}/comments`,
+                state: {
+                  story,
+                  commentsIds: kids,
+                },
+              }}
+            >
+              {kids.length} comments
+            </Link>
+          </>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Story;
