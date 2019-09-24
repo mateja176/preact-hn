b6c7dc449155104ebd1badadb576b2a04b6cf99c
Mateja Petrovic <ma.petrovic@levi9.com>
2019-09-24 18:02:44 +0200

    feat(App): adds Jumbotron

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import { Jumbotron } from 'react-bootstrap';
 import { hot } from 'react-hot-loader';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Link } from 'react-router-dom';
 import Routes from './Routes';
 
 const App: React.FC = () => (
   <BrowserRouter>
-    <div style={{ margin: 20 }}>
-      <h1 style={{ marginBottom: 20 }}>Hacker news</h1>
+    <Jumbotron style={{ minHeight: '100vh', marginBottom: 0 }}>
+      <Link to="/">
+        <h1>Hacker news</h1>
+      </Link>
+      <hr />
       <Routes />
-    </div>
+    </Jumbotron>
   </BrowserRouter>
 );
 
