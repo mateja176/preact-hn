6f3ff1e36f2d04c7e9e7e1dd9322385be0e03976
Mateja Petrovic <ma.petrovic@levi9.com>
2019-09-24 12:20:53 +0200

    feat(App): enables hot reloading

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { hot } from 'react-hot-loader';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './Routes';
 
@@ -10,4 +11,4 @@ const App: React.FC = () => (
   </BrowserRouter>
 );
 
-export default App;
+export default hot(module)(App);
